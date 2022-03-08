f37e0836a473b89af8aff09355000ad9081d4b63
JManuelHeredia <60076532+JManuelHeredia@users.noreply.github.com>
2022-03-07 23:13:54 -0600

    Testing Roman numerals. Not fixed at all

--- src/helpers/string-parser.ts
+++ src/helpers/string-parser.ts
@@ -5,10 +5,12 @@ export function stringCleaner( title:string ): string {
     //Remove/Ignore track numbers in the title with a format like this: "04 Title"
     newTitle.map(word => (word.match(/[a-zA-Z]/g)) ? word : '');
 
-
+    //
+    const romanNumerals:RegExp = new RegExp(/(?<![MDCLXVI])(?=[MDCLXVI])M{0,3}(?:C[MD]|D?C{0,3})(?:X[CL]|L?X{0,3})(?:I[XV]|V?I{0,3})[^ ]\b/i);
+    //
     newTitle = newTitle.map(word =>{
         const startFrom = (word.charAt(0).match(/\W/)) ? 1 : 0;
-        if(word.match(/[i|v|x]{1,3}/i) && word.length < 4) return word.toUpperCase();
+        if(word.match(romanNumerals) && word.length < 4) return word.toUpperCase();
         return `${ (startFrom == 1) ? word.charAt(0): ''}${ word[startFrom].toUpperCase() + word.substring(startFrom + 1).toLowerCase()}`;
     });
     console.log(newTitle.join(' '));
