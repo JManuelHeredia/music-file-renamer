a2cc4dd84f5ebc03acab512e7646981c14d8c622
JManuelHeredia <60076532+JManuelHeredia@users.noreply.github.com>
2022-03-07 22:17:40 -0600

    Ignore Roman numerals and parenthesis for cases like covers/feat

--- src/helpers/string-parser.ts
+++ src/helpers/string-parser.ts
@@ -2,10 +2,15 @@ export function stringCleaner( title:string ): string {
     if(!title) return '';
     let newTitle:string[] = title.trim().split(' ');
 
+    //Remove/Ignore track numbers in the title with a format like this: "04 Title"
+    newTitle.map(word => (word.match(/[a-zA-Z]/g)) ? word : '');
 
-    newTitle.map(word => (word.match(/[a-zA-Z]/g)) ? word : '')
 
-    newTitle = newTitle.map(word => `${word[0].toUpperCase() + word.substring(1).toLowerCase()}`);
-
+    newTitle = newTitle.map(word =>{
+        const startFrom = (word.charAt(0).match(/\W/)) ? 1 : 0;
+        if(word.match(/[i|v|x]{1,3}/i) && word.length < 4) return word.toUpperCase();
+        return `${ (startFrom == 1) ? word.charAt(0): ''}${ word[startFrom].toUpperCase() + word.substring(startFrom + 1).toLowerCase()}`;
+    });
+    console.log(newTitle.join(' '));
     return newTitle.join(' ');
 }
