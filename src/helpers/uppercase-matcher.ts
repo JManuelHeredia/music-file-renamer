59f6b8a2ff99c7efadf83626ec1402b92c389c05
JManuelHeredia <60076532+JManuelHeredia@users.noreply.github.com>
2022-03-07 20:58:23 -0600

    Updated uppercase matcher

---
+++ src/helpers/uppercase-matcher.ts
@@ -0,0 +1,8 @@
+export const uppercaseMatch = ( songData:string ):boolean => {
+    let counter:number = 0;
+    for(let i = 1; i < songData.length; i++){
+        if(songData.charCodeAt(i) > 64 && songData.charCodeAt(i) < 91) counter++;
+        if(counter > 2 && songData.length > 2) return true;
+    }
+    return false;
+}
