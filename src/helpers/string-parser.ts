ca2b90d379926e8f06f40a2b95ab08c1d588ceda
JManuelHeredia <60076532+JManuelHeredia@users.noreply.github.com>
2022-06-25 23:15:22 -0500

    Removed MISSING_DATA labels.

--- src/helpers/string-parser.ts
+++ src/helpers/string-parser.ts
@@ -1,18 +1,28 @@
-export function titleCaseParser( title:string ): string {
-    if(!title) return '';
-    let newTitle:string[] = title.trim().replace(':', ';').split(' '); //TODO: replace ':' for another char
+
+// const romanNumerals:RegExp = new RegExp(/(?<![MDCLXVI])(?=[MDCLXVI])M{0,3}(?:C[MD]|D?C{0,3})(?:X[CL]|L?X{0,3})(?:I[XV]|V?I{0,3})[^ ]\b/i);
+export const romanNumerals:RegExp = new RegExp(/^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/i);
+
+/**
+ *
+ * @param { String } tagStringToParse i.e. 'soy un /pescadito '
+ * @returns { String } i.e. 'Soy Un Pescadito'
+ */
+export function titleCaseParser( tagStringToParse:string ): string {
+    if(!tagStringToParse) return '';
+
+    let newTitle:string[] = tagStringToParse.trim().replace(':', ';').replace('/', '').replace('\\', '').split(' '); //TODO: replace ':' for another char
 
     //Remove/Ignore track numbers in the title with a format like this: "04 Title"
     newTitle.map(word => (word.match(/[a-zA-Z]/g)) ? word : '');
 
-    //
-    // const romanNumerals:RegExp = new RegExp(/(?<![MDCLXVI])(?=[MDCLXVI])M{0,3}(?:C[MD]|D?C{0,3})(?:X[CL]|L?X{0,3})(?:I[XV]|V?I{0,3})[^ ]\b/i);
-    const romanNumerals:RegExp = new RegExp(/^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/i);
-    //
-    newTitle = newTitle.map(word =>{
-        const startFrom = (word.charAt(0).match(/\W/)) ? 1 : 0;
-        if(word.match(romanNumerals) && word.length < 4) return word.toUpperCase();
-        return `${ (startFrom == 1) ? word.charAt(0): ''}${ word[startFrom].toUpperCase() + word.substring(startFrom + 1).toLowerCase()}`;
-    });
+    try {
+        newTitle = newTitle.map(word =>{
+            const startFrom = (word.charAt(0).match(/\W/)) ? 1 : 0;
+            if(word.match(romanNumerals) && word.length < 4) return word.toUpperCase();
+            return `${ (startFrom == 1) ? word.charAt(0): ''}${ word[startFrom].toUpperCase() + word.substring(startFrom + 1).toLowerCase()}`;
+        });
+    } catch (error) {
+        console.log('Error parsing: ', tagStringToParse);
+    }
     return newTitle.join(' ');
 }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import fs from 'fs';
 import NodeID3 from 'node-id3';
+
 import { titleCaseParser } from './helpers/string-parser';
 import { uppercaseMatch } from './helpers/uppercase-matcher';
 
 function main(): void{
     const argPath:string = process.argv[2].toString();
-
+    // Provided Folder/Path is valid
     if(!fs.existsSync(argPath)){
         console.log(`Invalid path ${argPath}... Exiting...`);
         return;
@@ -19,34 +20,36 @@ function main(): void{
         console.log('No mp3 files in this folder');
         return;
     }
-    // Get files metatags
+    // NodeID3 Options: Get selected metatags
     const options:object = {
-        include: ['TALB', 'TIT2', 'TPE1'],
+        include: ['TALB', 'TIT2', 'TPE1'], // Album, Title, Artist
         noraw:   true
     }
 
+    // Loop each file in [/path/file] arr
     files.forEach(file => {
         const filename:string =  `${argPath}\\${file}`;
         const tags:any = NodeID3.read(`${argPath}\\${file}`, options);
         let { album, title, artist }: { album:string; title:string; artist:string  } = { ...tags };
         // console.log(tags);
         if(!title){
-            console.warn(filename, "data missing: Title");
+            console.warn(file, "data missing: Title");
             // title = '[MISSING_TITLE]';
         }
         if(!artist){
-            console.warn(filename, "data missing: Artist");
+            console.warn(file, "data missing: Artist");
             // artist = '[MISSING_ARTIST]';
         }
-        // Sanitize and trim file name
-        const newTitle:string  = titleCaseParser(title)  || '[MISSING_TITLE]';
-        const newArtist:string = titleCaseParser(artist) || '[MISSING_ARTIST]';
+        // Sanitize and trim tag strings
+        const newTitle:string   = title  ? titleCaseParser(title)  : file.substring( 0, ( file.length - 4 ));
+        const newArtist:string  = artist ? titleCaseParser(artist) : artist;
+        const newAlbum:string   = album  ? titleCaseParser(album)  : album;
 
         //Remove Uppercase for MetaTags
         if(uppercaseMatch(title) || title !== newTitle){
             NodeID3.update(
                 {
-                    title: newTitle
+                    title: newTitle ?? title
                 },
                 filename
             );
@@ -54,7 +57,7 @@ function main(): void{
         if(uppercaseMatch(artist) || artist !== newArtist){
             NodeID3.update(
                 {
-                    artist: newArtist
+                    artist: newArtist ?? artist
                 },
                 filename
             );
@@ -62,21 +65,27 @@ function main(): void{
         if(uppercaseMatch(album) || album != titleCaseParser(album)){
             NodeID3.update(
                 {
-                    album: titleCaseParser(album) || ''
+                    album: newAlbum ?? album
                 },
                 filename
             );
         }
 
+        // New expected filename. i.e. 'Pescadito-Los Pescaditos.mp3'
         const newFilename:string = `${argPath}\\${newTitle}-${newArtist}.mp3`;
+
         //
-        if(filename == newFilename){
+        if(filename === newFilename){
             console.log(`${filename}`, 'not renamed, file already has the expected name.');
+            return;
         }
         else{
             // Rename files that can be renamed
             fs.rename(filename, newFilename, (err) => {
-                if(err) throw err;
+                if(err) {
+                    console.log( 'Error renaming:', file);
+                    throw err;
+                }
                 console.log(filename, "renamed succefully!");
             });
         }
