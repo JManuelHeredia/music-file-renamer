4f995f14afc38b21a854e3bf7193ec99f3268c88
JManuelHeredia <60076532+JManuelHeredia@users.noreply.github.com>
2022-06-25 23:14:26 -0500

    Added some validations and docs comments

--- src/helpers/uppercase-matcher.ts
+++ src/helpers/uppercase-matcher.ts
@@ -1,4 +1,15 @@
+import { romanNumerals } from "./string-parser";
+
+/**
+ *
+ * @param { Sting } songData [ 'AlbumName', 'Artist or Band', 'Title' ]
+ * @returns { Boolean } Has a lot of Uppercase letters but not a Roman Numeral?
+ */
+
 export const uppercaseMatch = ( songData:string ):boolean => {
+    if( !songData ) return false;
+
+    if( romanNumerals.test( songData )) return false;
     let counter:number = 0;
     for(let i = 1; i < songData.length; i++){
         if(songData.charCodeAt(i) > 64 && songData.charCodeAt(i) < 91) counter++;
